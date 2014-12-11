c05bec68e0284ce4154b863a6eb4c66397854c3c
unknown <DanDevlin19@dandevlin19.MaristSchool.Local>
2014-12-11 09:20:14 -0500

    Added DevlinBlink

---
+++ ArduinoSketches/devlinBlink/devlinBlink.ino
@@ -0,0 +1,19 @@
+//Daniel Devlin
+//Led Blink Program
+
+
+void blink() {
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+  delay(1000);  
+}
+
+void setup() {
+  pinMode(9, OUTPUT); 
+}
+
+void loop() {
+  blink();
+}
+
